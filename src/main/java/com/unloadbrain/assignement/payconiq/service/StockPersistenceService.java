ba17e8abf2e6afa4ef7b1d95827241f6c5d60d50
Mossaddeque Mahmood <mmahmood.ict.bd@gmail.com>
2019-07-21 13:38:15 +0200

    Exception handling mechanism.

--- src/main/java/com/unloadbrain/assignement/payconiq/service/StockPersistenceService.java
+++ src/main/java/com/unloadbrain/assignement/payconiq/service/StockPersistenceService.java
@@ -8,6 +8,7 @@ import com.unloadbrain.assignement.payconiq.dto.response.IdentityResponse;
 import com.unloadbrain.assignement.payconiq.dto.response.StockResponse;
 import com.unloadbrain.assignement.payconiq.dto.response.StocksResponse;
 import com.unloadbrain.assignement.payconiq.exception.StockNotFoundException;
+import com.unloadbrain.assignement.payconiq.util.DateTimeUtil;
 import com.unloadbrain.assignement.payconiq.util.UuidUtil;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -15,7 +16,6 @@ import org.springframework.data.cassandra.core.query.CassandraPageRequest;
 import org.springframework.data.domain.Slice;
 import org.springframework.stereotype.Service;
 
-import java.time.Instant;
 import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
@@ -28,16 +28,11 @@ public class StockPersistenceService {
 
     private StockRepository stockRepository;
     private UuidUtil uuidUtil;
+    private DateTimeUtil dateTimeUtil;
 
     public StocksResponse getStocks(int pageNo, int pageSize) {
 
-        Slice<Stock> slice = stockRepository.findAll(CassandraPageRequest.first(pageSize));
-
-        int currentPage = 0;
-        while (slice.hasNext() && currentPage < pageNo) {
-            slice = stockRepository.findAll(slice.nextPageable());
-            currentPage++;
-        }
+        Slice<Stock> slice = getPage(pageNo, pageSize);
 
         List<StockResponse> stockResponseList = slice.getContent().stream()
                 .map(stock -> buildStockResponse(stock))
@@ -47,6 +42,34 @@ public class StockPersistenceService {
                 .data(stockResponseList)
                 .pageNo(pageNo)
                 .pageSize(pageSize)
+                .build();
+    }
+
+    private Slice<Stock> getPage(int pageNo, int pageSize) {
+
+        /**
+         * For some reason, CassandraRepository can not find result page unless it's first page.
+         * Found following hack online.
+         */
+        Slice<Stock> slice = stockRepository.findAll(CassandraPageRequest.first(pageSize));
+
+        int currentPage = 0;
+        while (slice.hasNext() && currentPage < pageNo) {
+            slice = stockRepository.findAll(slice.nextPageable());
+            currentPage++;
+        }
+
+        return slice;
+    }
+
+
+    private StockResponse buildStockResponse(Stock stock) {
+
+        return StockResponse.builder()
+                .id(stock.getId().toString())
+                .name(stock.getName())
+                .currentPrice(stock.getCurrentPrice())
+                .lastUpdate(stock.getLastUpdate())
                 .build();
     }
 
@@ -61,7 +84,6 @@ public class StockPersistenceService {
         return buildStockResponse(existingStock.get());
     }
 
-
     public StockResponse updateStock(UpdateStockRequest updateStockRequest) {
 
         Optional<Stock> existingStock = stockRepository.findById(updateStockRequest.getId());
@@ -73,7 +95,7 @@ public class StockPersistenceService {
         Stock stock = existingStock.get();
         stock.setName(updateStockRequest.getName());
         stock.setCurrentPrice(updateStockRequest.getCurrentPrice());
-        stock.setLastUpdate(Instant.now().toEpochMilli());
+        stock.setLastUpdate(dateTimeUtil.getCurrentTimeEpochMilli());
 
         stockRepository.save(stock);
 
@@ -86,21 +108,11 @@ public class StockPersistenceService {
                 .id(UUID.fromString(uuidUtil.getRandomUuid()))
                 .name(createStockRequest.getName())
                 .currentPrice(createStockRequest.getCurrentPrice())
-                .lastUpdate(Instant.now().toEpochMilli())
+                .lastUpdate(dateTimeUtil.getCurrentTimeEpochMilli())
                 .build();
 
         stockRepository.save(stock);
 
         return IdentityResponse.builder().id(stock.getId().toString()).build();
-    }
-
-    private StockResponse buildStockResponse(Stock stock) {
-
-        return StockResponse.builder()
-                .id(stock.getId().toString())
-                .name(stock.getName())
-                .currentPrice(stock.getCurrentPrice())
-                .lastUpdate(stock.getLastUpdate())
-                .build();
     }
 }
