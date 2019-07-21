f51f9274d8d20466e7709002cc5520af1859af79
Mossaddeque Mahmood <mmahmood.ict.bd@gmail.com>
2019-07-21 22:56:55 +0200

    Remove bean level validation for now.

--- src/main/java/com/unloadbrain/assignement/payconiq/stockapi/dto/reqeust/UpdateStockRequest.java
+++ src/main/java/com/unloadbrain/assignement/payconiq/stockapi/dto/reqeust/UpdateStockRequest.java
@@ -5,10 +5,7 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-import javax.validation.constraints.DecimalMin;
-import javax.validation.constraints.Digits;
 import javax.validation.constraints.NotNull;
-import javax.validation.constraints.Pattern;
 import java.math.BigDecimal;
 
 @Data
@@ -20,11 +17,8 @@ public class UpdateStockRequest {
     private String id;
 
     @NotNull
-    @Pattern(regexp = "^\\w+$", message = "Name has to be alphanumeric.}")
     private String name;
 
     @NotNull
-    @Digits(integer = 12, fraction = 2, message = "Current price has a max limit of 12 integer and 2 fraction digit.")
-    @DecimalMin(value = "0", message = "Current price has to be positive.")
     private BigDecimal currentPrice;
 }
